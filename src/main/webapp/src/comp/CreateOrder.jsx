80fd6bb2964dc0c172a4db0fa5f6872efa5db2e9
ADdad <rokden80@gmail.com>
2018-11-11 19:16:30 +0200

    Added attributes render

--- src/main/webapp/src/comp/CreateOrder.jsx
+++ src/main/webapp/src/comp/CreateOrder.jsx
@@ -1,28 +1,53 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { RadioGroup, Radio } from "react-radio-group";
+import { Checkbox, CheckboxGroup } from "react-checkbox-group";
 class CreateOrder extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: "",
-      fields: [],
       myItems: [
         { id: "2das", name: "model1, producer1" },
         { id: "5das", name: "model2, producer1" }
       ],
       resultItems: [{ itemId: 0, quantity: 0 }],
-      item: {},
       warehouses: [1, 2, 3, 4, 5],
-      items: [1, 2, 3, 4, 5],
-      num: 1,
-      roles: ["User", "Keeper", "Admin"],
-      Warehouse: 0,
+      warehouse: 0,
       type: "",
-      creationDate: "",
-      updatedDate: "",
       description: "",
       attachments: [],
-      comments: []
+      optionalAttributes: [
+        {
+          name: "TestRadio",
+          type: "radio",
+          multiple: true,
+          immutable: false,
+          values: ["Test1", "Test2", "Test3"]
+        },
+        {
+          name: "TestCheckbox",
+          type: "checkbox",
+          multiple: true,
+          immutable: false,
+          values: ["Test1", "Test2", "Test3"]
+        },
+        {
+          name: "TestDate",
+          type: "date",
+          multiple: false,
+          immutable: false,
+          values: []
+        },
+        {
+          name: "TestText",
+          type: "Text",
+          multiple: false,
+          immutable: false,
+          values: []
+        }
+      ],
+      oAttributesValues: []
     };
   }
 
@@ -41,29 +66,125 @@ class CreateOrder extends Component {
     this.setState({ resultItems: readyItems });
   };
 
-  renderAdditionalAttribute = (type, name, multiple, values, immutable) => {
-    // switch(type) {
-    //   case "select": {
-    //       return (
-    //         <div className="form-row">
-    //         <div className="form-group">
-    //         </div>
-    //         </div>
-    //       );
-    //   }
-    //   case "textarea": {
-    //     <div className="form-row">
-    //     <div className="form-group">
-    //     </div>
-    //     </div>
-    //   }
-    //   default: {
-    //     <div className="form-row">
-    //     <div className="form-group">
-    //     </div>
-    //     </div>
-    //   }
-    // }
+  renderAdditionalAttribute = (
+    type,
+    name,
+    multiple,
+    values,
+    immutable,
+    index
+  ) => {
+    switch (type) {
+      case "select": {
+        return (
+          <div className="form-row">
+            <div className="form-group">
+              <label>{name}: </label>
+              <select
+                className="form-control"
+                onChange={p => this.changeOptionalValue(index, p.target.value)}
+              >
+                {values.map(p => (
+                  <option key={p} value={p}>
+                    {p}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        );
+      }
+      case "textarea": {
+        return (
+          <div className="form-row">
+            <div className="form-group col-md-8">
+              <label>{name}</label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="5"
+                onChange={p => this.changeOptionalValue(index, p.target.value)}
+              />
+            </div>
+          </div>
+        );
+      }
+      case "radio": {
+        return (
+          <div className="form-row">
+            <div className="form-group">
+              <h4>{name}</h4>
+              <RadioGroup name={name} onChange={this.changeRadiobuttonValue}>
+                {values.map(v => (
+                  <label key={v} className="form-group m-2">
+                    <Radio value={v + "|" + index} />
+                    {v}
+                  </label>
+                ))}
+              </RadioGroup>
+            </div>
+          </div>
+        );
+      }
+      case "checkbox": {
+        return (
+          <div className="form-row">
+            <div className="form-group">
+              <h4>{name}</h4>
+              <CheckboxGroup
+                checkboxDepth={2}
+                name={name}
+                onChange={this.handleCheckbox}
+              >
+                {values.map(v => (
+                  <label key={v} className="form-group m-2">
+                    <Checkbox value={v + "|" + index} />
+                    {v}
+                  </label>
+                ))}
+              </CheckboxGroup>
+            </div>
+          </div>
+        );
+      }
+      default: {
+        return (
+          <div className="form-row">
+            <div className="form-group">
+              <h4>{name}: </h4>
+              <input
+                className="form-control col-md-12"
+                type={type}
+                onChange={p => this.changeOptionalValue(index, p.target.value)}
+              />
+            </div>
+          </div>
+        );
+      }
+    }
+  };
+
+  changeOptionalValue = (index, value) => {
+    let optionalVal = this.state.oAttributesValues.slice();
+    optionalVal[index] = value;
+    this.setState({ oAttributesValues: optionalVal });
+  };
+  changeRadiobuttonValue = value => {
+    let tempVal = value;
+    let temp = tempVal.split("|");
+    let resValue = temp[0];
+    let index = temp[1];
+    this.changeOptionalValue(index, resValue);
+  };
+  handleCheckbox = value => {
+    let checkBoxValues = "";
+    let index = 0;
+    for (let i = 0; i < value.length; i++) {
+      let temp = value[i].split("|");
+      checkBoxValues += temp[0] + "|";
+      index = temp[1];
+    }
+    this.changeOptionalValue(index, checkBoxValues);
   };
 
   handleAdd = () => {
@@ -100,6 +221,21 @@ class CreateOrder extends Component {
       .catch(error => console.error("Error:", error));
   };
   render() {
+    const globalOptionalFields = [...this.state.optionalAttributes];
+    let localOptionalFields = [];
+    for (let i = 0; i < this.state.optionalAttributes.length; i++) {
+      localOptionalFields.push(
+        this.renderAdditionalAttribute(
+          globalOptionalFields[i].type,
+          globalOptionalFields[i].name,
+          globalOptionalFields[i].multiple,
+          globalOptionalFields[i].values,
+          globalOptionalFields[i].immutable,
+          i
+        )
+      );
+    }
+
     let items = [];
     for (let i = 0; i < this.state.resultItems.length; i++) {
       items.push(
@@ -165,7 +301,7 @@ class CreateOrder extends Component {
                 <label>Warehouse</label>
                 <select
                   className="form-control"
-                  onChange={e => this.setState({ Warehouse: e.target.value })}
+                  onChange={e => this.setState({ warehouse: e.target.value })}
                 >
                   {this.state.warehouses.map(p => (
                     <option key={p} value={p}>
@@ -204,6 +340,7 @@ class CreateOrder extends Component {
                 />
               </label>
             </div>
+            {localOptionalFields}
             <div className="form-row">
               <button
                 className="form-group col-md-3 btn btn-lg btn-outline-success"
