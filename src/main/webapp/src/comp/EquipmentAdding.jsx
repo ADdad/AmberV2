57a08f574127134600d167fee40e3f5faed5e122
ADdad <rokden80@gmail.com>
2018-12-11 14:47:32 +0200

    Edited role validation

--- src/main/webapp/src/comp/EquipmentAdding.jsx
+++ src/main/webapp/src/comp/EquipmentAdding.jsx
@@ -8,6 +8,7 @@ class EquipmentAdding extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      userRoles: [],
       warehouses: [],
       warehouseId: "",
       viewItems: [],
@@ -24,7 +25,12 @@ class EquipmentAdding extends Component {
     fetch("/userinfo")
       .then(response => response.json())
       .then(data => {
-        this.setState({ userId: data.id });
+        this.setState({
+          userRoles: data.roles.map(role => role.name),
+          isLoading: false
+        });
+        if (!this.state.userRoles.includes("ROLE_ADMIN"))
+          this.props.history.push("/errorpage");
       })
       .catch(error => console.log(error));
     fetch(`/equipment`, {
